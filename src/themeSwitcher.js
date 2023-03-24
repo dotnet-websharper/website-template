b18a2bfe2c66dfc0670d68a0b4c1890e7122ea7f
Meschac Irung <meschacirung@gmail.com>
2023-03-24 10:05:46 +0200

    fix theme switcher

--- src/themeSwitcher.js
+++ src/themeSwitcher.js
@@ -16,6 +16,14 @@ switchers.forEach((switcher) => {
                 document.documentElement.classList.remove("dark");
                 localStorage.setItem("color-theme", "light");
             }
+        } else {
+            if (document.documentElement.classList.contains("dark")) {
+                document.documentElement.classList.remove("dark");
+                localStorage.setItem("color-theme", "light");
+            } else {
+                document.documentElement.classList.add("dark");
+                localStorage.setItem("color-theme", "dark");
+            }
         }
     });
 });
