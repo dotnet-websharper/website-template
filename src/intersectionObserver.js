5323b443897d753ca4861089dfe8a730c7fb45af
Meschac Irung <meschacirung@gmail.com>
2023-03-28 16:07:55 +0200

    fix [io] : fix rootMargin

--- src/intersectionObserver.js
+++ src/intersectionObserver.js
@@ -1,24 +1,21 @@
 let links = document.querySelectorAll(".section-link");
 let sections = document.querySelectorAll(".section");
-
 let options = {
     root: null,
-    threshold: 0,
-    rootMargin: "-410px",
+    threshold: 1.0,
+    rootMargin: "0px",
 };
-
 function activate(link) {
     link.classList.add("active-link");
 }
 
-const observer = new IntersectionObserver(function (entries, observes) {
+const observer = new IntersectionObserver(function (entries) {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             links.forEach((link) => {
                 let linkTarget = link.getAttribute("data-step");
                 let entryId = entry.target.getAttribute("id");
-
-                linkTarget == entryId ? activate(link) : link.classList.remove("active-link");
+                linkTarget === entryId ? activate(link) : link.classList.remove("active-link");
             });
         }
     });
