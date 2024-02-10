36aeedf58a28425f0dd8abf5a607042cef52c524
Meschac Irung <meschacirung@gmail.com>
2024-02-10 19:54:44 +0200

    build signin page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./support.html" ,"./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   safelist: ["h-screen", "hidde-open-icon", "visible-close-icon"],
   darkMode : "class",
   theme: {
