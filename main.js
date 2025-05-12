b5d9ec848527455a78266f1ba38d5a8f48bec183
Got <souvanxay38@gmail.com>
2025-05-12 15:06:34 +0200

    add snippet for websharper.ui

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import "./src/intersectionObserver";
 import "./src/themeSwitcher";
 import "./src/prism.js";
-import "./src/counter";
 import "./src/navmenu";
 import "./src/clipboard";
 import "./src/tabs";
