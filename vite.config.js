fc8da9d871f173a7d2e9e9219fc9e90792e38a4d
Got <souvanxay38@gmail.com>
2025-05-19 15:18:04 +0200

    fix error on ghpages.yml and update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     build: {
+        outDir: "dist",
+        base: '/website-template/',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                support: resolve(__dirname, "support.html"),
-                signin: resolve(__dirname, "signin.html"),
-                register: resolve(__dirname, "register.html"),
-                checkout: resolve(__dirname, "checkout.html")
+                support: resolve(__dirname, "pages/support.html"),
+                signin: resolve(__dirname, "pages/signin.html"),
+                register: resolve(__dirname, "pages/register.html"),
+                checkout: resolve(__dirname, "pages/checkout.html")
             },
         },
     },
