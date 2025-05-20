0506d3d23604462fc532f8e453157380c5f795f4
Got <souvanxay38@gmail.com>
2025-05-20 13:59:31 +0200

    Update clipboard.js to be able to copy difference text

--- src/clipboard.js
+++ src/clipboard.js
@@ -1,32 +1,35 @@
 let copyBtns = document.querySelectorAll(".copy-btn");
 
-async function copyContent(copyIcon, successIcon, errorIcon) {
+async function copyContent(textToCopy, copyIcon, successIcon, errorIcon) {
     try {
-        await navigator.clipboard.writeText("dotnet new -i WebSharper.Templates");
+        await navigator.clipboard.writeText(textToCopy);
         copyIcon.classList.add("hidden");
         successIcon.classList.remove("hidden");
-        setTimeout(
-            function () {
-                copyIcon.classList.remove("hidden");
-                successIcon.classList.add("hidden");
-            }.bind(this),
-            1000
-        );
+        setTimeout(() => {
+            copyIcon.classList.remove("hidden");
+            successIcon.classList.add("hidden");
+        }, 1000);
     } catch (err) {
         copyIcon.classList.add("hidden");
         errorIcon.classList.remove("hidden");
-        setTimeout(
-            function () {
-                copyIcon.classList.remove("hidden");
-                errorIcon.classList.add("hidden");
-            }.bind(this),
-            1000
-        );
+        setTimeout(() => {
+            copyIcon.classList.remove("hidden");
+            errorIcon.classList.add("hidden");
+        }, 1000);
     }
 }
 
 copyBtns.forEach((copyBtn) => {
     copyBtn.addEventListener("click", () => {
-        copyContent(copyBtn.querySelector(".copy"), copyBtn.querySelector(".success"), copyBtn.querySelector("failed"));
+        const container = copyBtn.closest("div");
+        const textElement = container.querySelector(".font-mono");
+        const textToCopy = textElement.innerText;
+
+        copyContent(
+            textToCopy,
+            copyBtn.querySelector(".copy"),
+            copyBtn.querySelector(".success"),
+            copyBtn.querySelector(".failed")
+        );
     });
 });
