d1328cdcbec522ac6eb7559494a9d1c88f4d6bc8
Got <souvanxay38@gmail.com>
2025-05-20 15:07:22 +0200

    add download page to vite config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
+                download: resolve(__dirname, "pages/download.html"),
                 support: resolve(__dirname, "pages/support.html"),
                 signin: resolve(__dirname, "pages/signin.html"),
                 register: resolve(__dirname, "pages/register.html"),
