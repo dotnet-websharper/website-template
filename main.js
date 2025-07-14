78ad12cf09b992f329b25ce071f1b53ee2cdc5d6
Got <souvanxay38@gmail.com>
2025-07-14 09:47:31 +0200

    change to use asciinema instead of vhs for ws-template vide

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ import "./src/prism.js";
 import "./src/navmenu";
 import "./src/clipboard";
 import "./src/tabs";
+import * as AsciinemaPlayer from 'asciinema-player';
+
+AsciinemaPlayer.create('ws-template.cast', document.getElementById('ws-template'), {
+  cols: 100,
+  rows: 20,
+  theme: 'dracula',
+  speed: 2.5,
+  autoPlay: true,
+  loop: true,
+  idleTimeLimit: 4
+});
