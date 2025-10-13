8138ec004659af28582c7e0602ebf699c8caa0d9
Got <souvanxay38@gmail.com>
2025-10-13 10:19:06 +0200

    move page-specific JS into their HTML; slim main.js to cut unused imports

--- main.js
+++ main.js
@@ -4,11 +4,7 @@ import "./src/prism.js";
 import "./src/navmenu";
 import "./src/clipboard";
 import "./src/tabs";
-// import "./src/videoPlayer.js"
 import "./src/lineNumbers";
-import "./src/headerSlides";
-import "./src/contactForm.js";
-import "./src/manageSubscription.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const el = document.getElementById('ws-template');
