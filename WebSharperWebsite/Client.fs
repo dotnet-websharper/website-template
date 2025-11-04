74cb6faea84b6fed1016723ed1f26f36f1129ede
Got <souvanxay38@gmail.com>
2025-11-04 21:27:37 +0100

    initial commit

---
+++ WebSharperWebsite/Client.fs
@@ -0,0 +1,25 @@
+﻿namespace WebSharperWebsite
+
+open WebSharper
+open WebSharper.UI
+open WebSharper.UI.Notation
+open WebSharper.UI.Templating
+
+[<JavaScript>]
+module Templates =   
+    type MainTemplate = Templating.Template<"Main.html", ClientLoad.FromDocument, ServerLoad.PerRequest>
+
+[<JavaScript>]
+module Client =
+    let DoSomething (input: string) =
+        System.String(Array.rev(input.ToCharArray()))
+
+    let Main () =
+        let rvReversed = Var.Create ""
+        Templates.MainTemplate.MainForm()
+            .OnSend(fun e ->
+                let res = DoSomething e.Vars.TextToReverse.Value
+                rvReversed := res
+            )
+            .Reversed(rvReversed.View)
+            .Doc()
