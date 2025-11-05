36323ad8354481ac406e77c565d633dd834d3f7b
Got <souvanxay38@gmail.com>
2025-11-05 16:35:17 +0100

    feat: add menu toggling

--- WebSharperWebsite/Src/Client.fs
+++ WebSharperWebsite/Src/Client.fs
@@ -18,9 +18,23 @@ module Client =
     let DoSomething (input: string) =
         System.String(Array.rev(input.ToCharArray()))
 
+    let byId (id: string) = JS.Document.GetElementById id
+
+    let ToggleMenu() =
+        let navmenu = byId "navmenu"
+        let openIcon = byId "openIcon"
+        let closeIcon = byId "closeIcon"
+        let bodyEl = JS.Document.Body
+
+        navmenu.ClassList.Toggle("h-screen") |> ignore
+        openIcon.ClassList.Toggle("hidden-open-icon") |> ignore
+        closeIcon.ClassList.Toggle("visible-close-icon") |> ignore
+        bodyEl.ClassList.Toggle("overflow-hidden") |> ignore
+
     let Layout () =
         Theme.Init()
         Templates.LayoutTemplate()
+            .ToggleMenu(fun _ -> ToggleMenu())
             .ToggleTheme(fun _ -> Theme.Toggle())
             .Bind()
         Doc.Empty
