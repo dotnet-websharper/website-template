575310f8fa378af62aff36fa70522993386a9cc1
Got <souvanxay38@gmail.com>
2025-11-17 14:51:08 +0100

    update: template-ify the invoices

--- WebSharperWebsite/Src/ManageSubscription/Views.Invoices.fs
+++ WebSharperWebsite/Src/ManageSubscription/Views.Invoices.fs
@@ -8,42 +8,32 @@ open WebSharper.UI.Client
 open Types
 open State
 
+open WebSharperWebsite
+
 [<JavaScript>]
 module ViewsInvoices =
-    let private invoiceRowV (key: string) (invV: View<InvoiceRecord>) : Doc =
+    let private invoicesModel =
+        ListModel.Create (fun (i: InvoiceRecord) -> i.id) state.invoices
+
+    let private invoiceRowV (key: string) (invoiceV: View<InvoiceRecord>) : Doc =
         let hrefV =
-            invV
+            invoiceV
             |> View.Map (fun inv ->
                 "./invoice.html?id="
                 + JS.EncodeURIComponent(inv.id)
                 + "&sub="
                 + JS.EncodeURIComponent(state.currentSubId))
 
-        tr [] [
-            td [ attr.``class`` "px-4 py-3" ] [
-                invV |> View.Map (fun i -> i.id) |> Doc.TextView
-            ]
-            td [ attr.``class`` "px-4 py-3" ] [
-                invV |> View.Map (fun i -> i.date) |> Doc.TextView
-            ]
-            td [ attr.``class`` "px-4 py-3" ] [
-                invV |> View.Map (fun i -> State.formatMoney i.amount i.currency) |> Doc.TextView
-            ]
-            td [ attr.``class`` "px-4 py-3 capitalize" ] [
-                invV |> View.Map (fun i -> i.status) |> Doc.TextView
-            ]
-            td [ attr.``class`` "px-4 py-3 text-right" ] [
-                a [
-                    attr.``class`` "rounded-md border px-2 py-1 text-xs border-gray-300 dark:border-white/20"
-                    Attr.Dynamic "href" hrefV
-                    attr.target "_blank"
-                    Attr.Create "rel" "noopener"
-                ] [ text "View" ]
-            ]
-        ]
-
-    let private invoicesModel =
-        ListModel.Create (fun (i: InvoiceRecord) -> i.id) state.invoices
+        Templates.ManageSubscriptionTemplate.InvoiceRow()
+            .InvoiceId(invoiceV |> View.Map (fun i -> i.id))
+            .Date(invoiceV |> View.Map (fun i -> i.date))
+            .Amount(
+                invoiceV
+                |> View.Map (fun i -> State.formatMoney i.amount i.currency)
+            )
+            .Status(invoiceV |> View.Map (fun i -> i.status))
+            .Href(hrefV)
+            .Doc()
 
     let private invoicesDoc =
         invoicesModel.View
